e103b37ae2a6ad457d2afbe71f636762aedbc14c
shoderico <yanagida@shoderitz.com>
2025-05-19 15:26:21 +0900

    iOS: PlatformService: omit permission request. and notify the IBluetooth.State changed

--- BlePermissionApp/Platforms/iOS/PlatformService.cs
+++ BlePermissionApp/Platforms/iOS/PlatformService.cs
@@ -4,59 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BlePermissionApp.Shared.Services;
+using Plugin.BLE;
 using Plugin.BLE.Abstractions.Contracts;
 
 namespace BlePermissionApp.Platforms
 {
     internal class PlatformService : PlatformServiceBase
     {
-        public override async Task<bool> CheckBluetoothPermissionAsync(Page page)
+        private IBluetoothLE _ble;
+
+        public PlatformService()
         {
+            _ble = CrossBluetoothLE.Current;
+            _ble.StateChanged += OnStateChanged;
+        }
+
+        private async void OnStateChanged(object? sender, Plugin.BLE.Abstractions.EventArgs.BluetoothStateChangedArgs e)
+        {
+            Page currentPage = Shell.Current;
+
             try
             {
-                if (BluetoothLE.State != BluetoothState.On)
+                // On iOS, IBluetoothLE.State returns BluetoothState.Unknown until IAdapter executes the first scan.
+                // Therefore, checking IBluetoothLE.State in the CheckBluetoothPermissionAsync does not work.
+                // So, we catch the StateChanged event and show the message.
+                switch (e.NewState)
                 {
-                    // Permissions.CheckStatusAsync<Permissions.Bluetooth>() always returns PermissionStatus.Granted.
-                    Console.WriteLine("Bluetooth is off, or Bluetooth permission is denied for this app");
-                    if (await page.DisplayAlert(
-                        "Cannot use Bluetooth",
-                        "Bluetooth is either turned off or the app's Bluetooth permission is not granted. Please turn on Bluetooth and grant Bluetooth permission from the app's settings.",
-                        "Go to app settings",
-                        "Cancel"
-                    ))
-                    {
-                        AppInfo.ShowSettingsUI();
-                    }
-                    return false;
-                }
-
-                var permissionStatus = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
-                if (permissionStatus != PermissionStatus.Granted)
-                {
-                    permissionStatus = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-                    if (permissionStatus != PermissionStatus.Granted)
-                    {
-                        if (await page.DisplayAlert(
-                            "Location permission required",
-                            "Location permission is required to use Bluetooth functionality. Please allow it from Settings.",
-                            "Go to Settings",
+                    case BluetoothState.Unavailable:
+                    case BluetoothState.Unauthorized:
+                    case BluetoothState.Off:
+                        Console.WriteLine("Bluetooth is off, or Bluetooth permission is denied for this app");
+                        if (await currentPage.DisplayAlert(
+                            "Cannot use Bluetooth",
+                            "Bluetooth is either turned off or the app's Bluetooth permission is not granted. Please turn on Bluetooth and grant Bluetooth permission from the app's settings." + $" State: {_ble.State}",
+                            "Go to app settings",
                             "Cancel"
                         ))
                         {
                             AppInfo.ShowSettingsUI();
                         }
-
-                        return false;
-                    }
+                        return;
                 }
             }
             catch (Exception ex)
             {
-                await page.DisplayAlert("Error", $"An error occurred while checking Bluetooth permission: {ex.Message}", "OK");
-                return false;
+                await currentPage.DisplayAlert("Error", $"An error occurred while checking Bluetooth permission: {ex.Message}", "OK");
+                return;
             }
+        }
 
-            return true;
+
+        public override Task<bool> CheckBluetoothPermissionAsync(Page page)
+        {
+            // iOS does not need the Permission of Location Service to use Bluetooth.
+
+            return Task.FromResult(true);
         }
     }
 }
